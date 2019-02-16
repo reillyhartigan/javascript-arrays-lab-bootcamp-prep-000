44d26131bfd1244cacdb73c46f67fd92828f46ed
sensible-state-8093 <reilly.hartigan@yale.edu>
2019-02-16 23:03:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 var kittens = ["Milo", "Otis", "Garfield"] //define your array here
-//var name = "Ralph"
 // Add your functions and code here
 function destructivelyAppendKitten(name) {
   return kittens.push(name)
