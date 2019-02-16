22d6f8d1cfcc7d614a570afb72217dd9fc82d393
sensible-state-8093 <reilly.hartigan@yale.edu>
2019-02-16 22:43:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-var kittens = [] //define your array here
-
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
+var name = "Ralph"
 // Add your functions and code here
+function destructivelyAppendKitten(name) {
+  return kittens.push(name)
+}
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name)
+}
+function destructivelyRemoveLastKitten(name) {
+return kittens.pop(name)
+}
+function destructivelyRemoveFirstKitten(name) {
+return kittens.shift(name)
+}
+function appendKitten(name) {
+  return [kittens...name]
+}
