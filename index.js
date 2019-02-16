b2795620e6e2c5c78937a0738d70df832e1d81a9
sensible-state-8093 <reilly.hartigan@yale.edu>
2019-02-16 22:56:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ function prependKitten(name) {
   return kittens = [...kittens, name]
 }
 function removeLastKitten() {
-
+return kittens.slice(0, kittens.length - 1)
 }
 function removeFirstKitten() {
-  return kittens.slice(0, kittens.length - 1)
+  return kittens.slice(1)
 }
